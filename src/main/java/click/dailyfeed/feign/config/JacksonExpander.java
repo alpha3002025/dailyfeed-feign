2c86e0b97393cb8c47a0b3855a239c233e184af5
alpha3002025 <alpha300uk@gmail.com>
2025-09-11 14:55:11 +0900

    fix (config) post feign

---
+++ src/main/java/click/dailyfeed/feign/config/JacksonExpander.java
@@ -0,0 +1,17 @@
+package click.dailyfeed.feign.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Param;
+
+public class JacksonExpander implements Param.Expander {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String expand(Object value) {
+        try {
+            return objectMapper.writeValueAsString(value);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to serialize object to JSON", e);
+        }
+    }
+}
--- src/main/java/click/dailyfeed/feign/domain/post/PostFeignClient.java
+++ src/main/java/click/dailyfeed/feign/domain/post/PostFeignClient.java
@@ -1,14 +1,18 @@
 package click.dailyfeed.feign.domain.post;
 
 import click.dailyfeed.code.domain.content.post.dto.PostDto;
+import click.dailyfeed.feign.config.JacksonExpander;
+import feign.Body;
 import feign.Headers;
+import feign.Param;
 import feign.RequestLine;
 import feign.Response;
 
 public interface PostFeignClient {
-
-    @RequestLine("GET /api/posts/list")
-    @Headers("Authorization: {token}")
-    Response getPostList(PostDto.PostsBulkRequest request, String token);
-
+    ///  bulk 조회 (특정 id list 에 해당하는 글의 목록 조회)
+    @RequestLine("POST /api/posts/list")
+    @Headers({"Content-Type: application/json", "Authorization: {token}"})
+    @Body("{request}")
+    Response getPostList(@Param(value = "request", expander = JacksonExpander.class) PostDto.PostsBulkRequest request,
+                        @Param("token") String token);
 }
