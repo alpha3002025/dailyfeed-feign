0191573e2ef9e168350e84186edfb88b66d88173
alpha3002025 <135424914+alpha3002025@users.noreply.github.com>
2025-10-07 11:59:59 +0900

    feat: k8s 전환작업
    - jib 빌드시 멀티 프로젝트 구조로 인해 중복 의존성으로 인해 깨지던 build 수정
    - 서브모듈로 전환

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,73 +1,36 @@
-plugins {
-	java
-	id("org.springframework.boot") version "3.5.5"
-	id("io.spring.dependency-management") version "1.1.7"
-}
-
-subprojects {
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
-}
-
 val springCloudVersion = "2025.0.0"
 val resilience4jVersion = "1.7.0"
 val openFeignVersion = "13.2.1"
 
-allprojects {
-    group = "click.dailyfeed"
-    version = "0.0.1-SNAPSHOT"
-    description = "Demo project for Spring Boot"
-
-    java {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(17)
-        }
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
     }
+}
 
-    configurations {
-        compileOnly {
-            extendsFrom(configurations.annotationProcessor.get())
-        }
-    }
+dependencies {
+    implementation(project(":dailyfeed-code"))
 
-    repositories {
-        mavenCentral()
-    }
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
-    dependencyManagement {
-        imports {
-            mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
-        }
-    }
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
-    dependencies {
-        implementation(project(":dailyfeed-code"))
+    // apache commons
+    implementation("commons-io:commons-io:2.11.0")
 
-        // spring
-        implementation("org.springframework.boot:spring-boot-starter-web")
+    // resilience4j
+    implementation("io.github.resilience4j:resilience4j-feign:${resilience4jVersion}")
+    // openfeign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("io.github.openfeign:feign-gson:${openFeignVersion}")
+    implementation("io.github.openfeign:feign-core:${openFeignVersion}")
+    implementation("io.github.openfeign:feign-jackson:${openFeignVersion}")
+    implementation("io.github.openfeign:feign-slf4j:${openFeignVersion}")
 
-        // lombok
-        compileOnly("org.projectlombok:lombok")
-        annotationProcessor("org.projectlombok:lombok")
-
-        // apache commons
-        implementation("commons-io:commons-io:2.11.0")
-
-        // resilience4j
-        implementation("io.github.resilience4j:resilience4j-feign:${resilience4jVersion}")
-        // openfeign
-        implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-        implementation("io.github.openfeign:feign-gson:${openFeignVersion}")
-        implementation("io.github.openfeign:feign-core:${openFeignVersion}")
-        implementation("io.github.openfeign:feign-jackson:${openFeignVersion}")
-        implementation("io.github.openfeign:feign-slf4j:${openFeignVersion}")
-
-        // test
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
